4eb2076bc830261481b650b11120fc4b9652ad37
Luca Sharbani <72736367+lucass0717@users.noreply.github.com>
2023-04-17 22:57:08 -0400

    resolved recipe dao conflict

--- src/recipes/recipes-dao.ts
+++ src/recipes/recipes-dao.ts
@@ -18,9 +18,5 @@ export const findRecipeById = async (recipeId: string) =>
   await recipesModel.findById(recipeId);
 
 // Retrieves a recipe by its api id
-<<<<<<< HEAD
 export const findRecipeByExternalId = async (recipeApiId: number) => 
-=======
-export const findRecipeByApiId = async (recipeApiId: number) => 
->>>>>>> 454bf2b0e6064bb73dcd983b756b717bdbb656ab
   await recipesModel.findOne({ recipeApiId });

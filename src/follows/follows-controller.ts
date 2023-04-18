c77fd6770da500b0f439d130e21fe3065e2e4441
lucass0717 <lucasharbani@gmail.com>
2023-04-18 02:58:21 -0400

    added error hadnling for follows

--- src/follows/follows-controller.ts
+++ src/follows/follows-controller.ts
@@ -1,28 +1,49 @@
 import * as followsDao from "./follows-dao";
+import { findUserById } from "../users/users-dao";
+import { follow } from "./follows-dao";
 
 function FollowsController(app) {
   const createFollow = async (req, res) => {
-    const follow = req.body;
-    const newFollow = await followsDao.createFollow(follow);
-    res.json(newFollow);
+    try {
+      const follow : follow = req.body;
+      const newFollow = await followsDao.createFollow(follow);
+      res.json(newFollow);
+    } catch (e) {
+      res.status(400).send("Invalid Follow");
+    }
   };
 
   const getFollowersById = async (req, res) => {
     const userId = req.params.userId;
+    const user = await findUserById(userId);
+    if (!user) {
+      res.status(404).send("User not found");
+      return;
+    }
     const followers = await followsDao.getFollowersById(userId);
     res.json(followers);
   };
 
   const getFollowingById = async (req, res) => {
     const userId = req.params.userId;
+    const user = await findUserById(userId);
+    if (!user) {
+      res.status(404).send("User not found");
+      return;
+    }
     const following = await followsDao.getFollowingById(userId);
     res.json(following);
   };
 
   const unfollow = async (req, res) => {
-    const follow = req.body;
-    const deletedFollow = await followsDao.unfollow(follow);
-    res.json(deletedFollow);
+    try {
+      const follow : follow = req.body;
+      const deletedFollow = await followsDao.unfollow(follow);
+      res.json(deletedFollow);
+    }
+    catch (e) {
+      res.status(400).send("Invalid Follow");
+    }
   };
 
   app.post("/api/follows", createFollow);

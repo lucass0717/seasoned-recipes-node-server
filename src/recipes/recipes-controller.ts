3b9ad8f8eca6b452d3723e5d6fa8715f5074d763
Luca Sharbani <72736367+lucass0717@users.noreply.github.com>
2023-04-17 22:58:06 -0400

    resolved recipe controller condlict

--- src/recipes/recipes-controller.ts
+++ src/recipes/recipes-controller.ts
@@ -2,19 +2,10 @@ import * as recipesDao from './recipes-dao';
 
 const RecipesController = (app) => {
   
-<<<<<<< HEAD
   // Get recipe by External Id
   const getRecipeById =  async (req, res) => {
     const recipeId = req.params.recipeId;
     const recipe = await recipesDao.findRecipeByExternalId(recipeId);
-=======
-  // Get recipe by Mongo Id
-  const getRecipeById =  async (req, res) => {
-    const recipeId = req.params.recipeId;
-    const recipe = await recipesDao.findRecipeById(recipeId);
->>>>>>> 454bf2b0e6064bb73dcd983b756b717bdbb656ab
-    res.json(recipe);
-  };
 
   // Save an external recipe in our database
   const createRecipe = async (req, res) => {

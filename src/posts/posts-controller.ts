306892e5e083e9c8443578f7fa87ced14ae329c6
Luca Sharbani <72736367+lucass0717@users.noreply.github.com>
2023-04-24 17:52:58 -0400

    liking and deleting implementation (#12)

--- src/posts/posts-controller.ts
+++ src/posts/posts-controller.ts
@@ -1,8 +1,6 @@
 import * as postsDao from "./posts-dao";
 import * as recipeDao from "../recipes/recipes-dao";
 import * as groupDao from "../groups/groups-dao";
-import {findRecipeByAPIId} from "../recipes/recipes-dao";
-import {getPostsInGroup} from "./posts-dao";
 
 const PostsController = (app) => {
 
@@ -21,8 +19,7 @@ const PostsController = (app) => {
   // Get all posts from people you follow
   async function getFollowedPosts(req, res) {
     const currentUser = req.session.currentUser;
-    // if no user is logged in, return 401
-    console.log("currentUser", currentUser);
+    // if no user is logged in, return 401=
     if (!currentUser) {
       res.status(401).json({ message: "Unauthorized" });
       return;
@@ -37,8 +34,6 @@ const PostsController = (app) => {
     const post = req.body.post;
     const recipe = req.body.recipe;
     let newPost = post;
-    console.log("post", post);
-    console.log("recipe", recipe);
 
     // if no user is logged in, return 401
     if (!currentUser) {
@@ -50,27 +45,30 @@ const PostsController = (app) => {
     if (!attachedRecipe) {
       attachedRecipe = await recipeDao.createRecipe(recipe);
     }
-    console.log(attachedRecipe);
     newPost = { ...newPost, recipeId: attachedRecipe._id};
 
     // Add the group to the post
     const groupId = await groupDao.getGroupByName(post.groupName);
-    console.log("groupId", groupId);
     newPost = { ...newPost, groupId};
 
     await postsDao.createPost(newPost);
-    res.json(newPost);
+    const populatedPost = await postsDao.getPostById(newPost._id);
+    res.json(populatedPost);
   }
 
   // Delete a post
   async function deletePost(req, res) {
     const postId = req.params.postId;
-    await postsDao.deletePost(postId);
+    const response = await postsDao.deletePost(postId);
+    if(response.deletedCount === 0) {
+      res.status(404).json({ message: "Post not found" });
+      return;
+    }
     res.json(postId);
   }
 
   app.get("/api/posts", getAllPosts);
-  app.get("/api/group-posts/:groupId", getGroupsPosts)
+  app.get("/api/group-posts/:groupId", getGroupsPosts);
   app.get("/api/followed-posts", getFollowedPosts);
   app.post("/api/posts", createPost);
   app.delete("/api/posts/:postId", deletePost);
